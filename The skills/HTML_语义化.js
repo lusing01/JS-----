f446253b7e4d0cfc0572e0c845616654f723e151
lusing01 <260072571@qq.com>
2018-09-05 18:04:07 +0800

    HTML 语义化修炼

---
+++ The skills/HTML_语义化.js
@@ -0,0 +1,20 @@
+// 为什么要 HTML 语义化？
+/*
+    根据内容的结构化，选择合适的标签。
+    1.便于开发者阅读和写出更优雅的代码。
+    2.浏览器和爬虫更好的解析。
+*/
+// 什么是语义化？
+var yuyihua = [{
+    "结构":"HTML", //
+    "表现":"CSS",
+    "行为":"JavaScript",
+}];
+/*
+    "结构-表现-行为"
+    "HTML-CSS-JavaScript"
+    结构： HTML 节点的层次、嵌套关系。
+    语义化就是让标签和其所包裹的内容的意思吻合
+*/
+// 对于 HTML 的标签的参考手册
+var href = "http://www.w3school.com.cn/tags/html_ref_byfunc.asp";
