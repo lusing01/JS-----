2e1359d2f28dc2395cf4fb8ced38f73ddc78bfde
lusing01 <260072571@qq.com>
2018-09-05 18:03:54 +0800

    前端技能表

---
+++ The skills/skillList.js
@@ -0,0 +1,20 @@
+/*
+语义化 HTML
+模块化 CSS
+ECMAScript
+原生 BOM 和 DOM(W3C/WHATWG)
+WebGl/Web Workers/WebSocket/Web Components/SVG
+WebKit/Blink
+兼容性
+jQuery UI 和 Bootstrap
+Grunt Less
+Meteor/AngularJS/Polymer
+Backbone.js/React/MV*
+Sea.js/RequireJS/Browserify/Bower/WebPack
+Less/Sass/PostCSS/CoffeeScript/TypeScript
+Grunt Gulp
+MongoDB/bash/ApacheBench
+ES7
+ECharts
+Zeu
+*/
