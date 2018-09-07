0e82ae7dae691099ddabb7fccb56b8d5a360a768
lusing01 <260072571@qq.com>
2018-09-07 20:44:13 +0800

    了解 W3C 标准

---
+++ The skills/W3C标准.js
@@ -0,0 +1,29 @@
+/*  HTML & CSS
+    HTML 和 CSS 是构建网页的两个核心技术。HTML 提供了网页的结构，CSS 是页面视觉和听觉的布局，针对不同的设备。
+    连同图形和脚本，HTML 和 CSS 是构建 Web 页面和应用的基础。
+*/
+/*  What is HTML?
+    HTML 是表达 Web 页面结构的语言。HTML 给前端工程师如下的帮助：
+    1.发表带有标题、文本、表格、列表、图像等的在线文档。
+    2.在线检索信息通过超文本链接，通过点击按钮。
+    3.为远程服务进行交易、搜索信息、预订座位、上网购物等设计表单。
+    4.直接将电子制表软件、视频音频剪辑和其他应用包含在文档中。
+    使用 HTML，前端工程通过 HTML 元素来码出页面的结构。
+    HTML 元素的标签有："paragraph"、"list"、"table"等。
+*/
+/* What is XHTML?
+    XHTML 是一种使用 XML 语法的 HTML 变体，扩展标记语言。
+    作为 HTML 的变体，XHTML 拥有和  HTML 一样的标签元素，但是语法上略有不同。
+    因为 XHTML 是 XML 的一种应用，可以和 XHTML 一起使用 XML 工具，就像 XSLT。
+*/
+/* What is CSS？
+    CSS 是一种用来修饰 Web 页面的语言，包含颜色，布局和字体。
+    CSS 允许兼容不同的设备。
+    CSS 是和 HTML 独立的，可以同任何 XML 类的标语言一起使用。
+    同 HTML 的分离，让维护站点，跨页面共享样式表，在不同的环境中修改页面。
+*/
+/*
+    HTML: 'https://www.w3.org/standards/techs/html#w3c_all'
+    CSS: 'https://www.w3.org/standards/techs/css#w3c_all'
+    Web Fonts: 'https://www.w3.org/standards/techs/webfonts#w3c_all'
+*/
